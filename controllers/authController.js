7306af8d86a70362bbcd0b9af129a4320755ce3c
moris96 <moriskhoudari@gmail.com>
2022-10-25 22:43:35 -0400

    created authentication controller

---
+++ controllers/authController.js
@@ -0,0 +1,27 @@
+
+
+// login 
+//POST 
+//sending data to the server 
+// sending a username and passwrod ---- Mongoose Method .findOne({ username: req.body.username })
+//lookup user in DB 
+//compare the password or credentials to the pass/cred of the foundUser 
+//if they match 
+// ---authenticate user 
+
+
+// singup 
+//POST
+//sending a username and password...more data
+
+
+// login View Route 
+//GET
+
+
+// singup view route 
+//GET
+
+
+//logout
+//GET
